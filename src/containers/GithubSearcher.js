37e3691ef93c6bbf304baa811307edd11db4612b
Kamil Gabryjelski <kamil.gabryjelski@eyzee.com>
2019-03-02 22:40:21 +0100

    added lodash and debouncing user input

--- src/containers/GithubSearcher.js
+++ src/containers/GithubSearcher.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import SearchMenu from '../components/SearchMenu';
 import axios from '../axios-setup';
+import debounce from 'lodash.debounce'
 
 class GithubSearcher extends Component {
     state = {
         repositories: []
     };
 
-    searchRepositories = (event) => {
-        const userInput = event.target.value;
+    searchRepositories = debounce((userInput) => {
         axios.get(`search/repositories?q=${userInput}`)
             .then(response => {
                     this.setState({
@@ -17,12 +17,12 @@ class GithubSearcher extends Component {
                     });
                 }
             );
-    };
+    }, 200);
 
     render() {
         return (
             <React.Fragment>
-                <SearchMenu onInputRepoNameChange={this.searchRepositories}/>
+                <SearchMenu onInputRepoNameChange={event => this.searchRepositories(event.target.value)}/>
                 {this.state.repositories.map(repo => (
                     <div key={repo.id}>{repo.name}</div>
                 ))}
