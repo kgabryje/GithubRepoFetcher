10445a558874dddefb8d05878340ac91ba115aeb
Kamil Gabryjelski <kamil.gabryjelski@eyzee.com>
2019-03-03 15:55:21 +0100

    some initial styling for RepoItems

--- src/components/Repos/RepoItem/RepoItem.js
+++ src/components/Repos/RepoItem/RepoItem.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary} from "@material-ui/core";
+import {
+    Avatar,
+    ExpansionPanel,
+    ExpansionPanelDetails,
+    ExpansionPanelSummary,
+    Icon,
+} from "@material-ui/core";
+import classes from './RepoItem.module.css';
+import Chip from "@material-ui/core/Chip";
 
 const RepoItem = props => (
     <ExpansionPanel>
-        <ExpansionPanelSummary>
-            <span>{props.name}</span>
-            <span>{props.owner.login}</span>
-            <span>{props.starsNumber}</span>
-            <span>{props.primaryLanguage}</span>
+        <ExpansionPanelSummary classes={{content: classes.Summary}}>
+
+            <div>{props.repository.name}</div>
+
+            <div style={{display: 'flex'}}>
+                <Avatar src={props.repository.owner.avatar_url}/>
+                <span style={{paddingLeft: '10px'}}>{props.repository.owner.login}</span>
+            </div>
+
+            <div className={classes.chip}>
+                <Chip icon={<Icon>star</Icon>}
+                      label={props.repository.stargazers_count}/>
+            </div>
+
+            <div className={classes.chip}>
+                {props.repository.language &&
+                <Chip label={props.repository.language}/>}
+            </div>
+
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
             {props.children}
@@ -17,10 +39,7 @@ const RepoItem = props => (
 );
 
 RepoItem.propTypes = {
-    name: PropTypes.string,
-    owner: PropTypes.object,
-    starsNumber: PropTypes.number,
-    primaryLanguage: PropTypes.string
+    repository: PropTypes.object
 };
 
 export default RepoItem;
