3e2cf2e9ec3a229453d795ec014a095d14235578
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2019-03-04 05:32:55 +0100

    Added forks icon

--- src/components/Repos/RepoDetails/RepoDetails.js
+++ src/components/Repos/RepoDetails/RepoDetails.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './RepoDetails.module.css';
-import {Chip, Icon, Tooltip} from '@material-ui/core';
+import {Chip, Icon, Tooltip, Typography} from '@material-ui/core';
+import ForkIcon from './ForkIcon';
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 const RepoDetails = props => (
     <div className={classes.RepoDetails}>
         {props.repository.description}
-        <Tooltip title='Watchers'>
-            <Chip icon={<Icon>remove_red_eye</Icon>} label={props.repository.watchers_count}/>
-        </Tooltip>
 
+        <div className={classes.AdditionalInfo}>
+            <Tooltip title='Watchers'>
+                <Chip icon={<Icon>remove_red_eye</Icon>} label={props.repository.watchers_count}/>
+            </Tooltip>
+
+            <Tooltip title='Forks'>
+                <Chip icon={<SvgIcon><ForkIcon/></SvgIcon>} label={props.repository.forks_count}/>
+            </Tooltip>
+        </div>
     </div>
 );
 
